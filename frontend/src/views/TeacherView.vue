f8c8c5847f1e932ebc355f827a307db1042b8806
laintime <hzhe111@163.com>
2022-09-11 10:41:41 +0800

    add teacher info tables

--- frontend/src/views/TeacherView.vue
+++ frontend/src/views/TeacherView.vue
@@ -1,7 +1,55 @@
 <template>
-  <div>
-    <h1>Teacher View</h1>
-    <p>{{ msg }}</p>
+  <div class="jumbotron vertical-center">
+    <div class="container">
+      <!-- bootswatch CDN-->
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/lumen/bootstrap.min.css"
+        integrity="sha384-GzaBcW6yPIfhF+6VpKMjxbTx6tvR/yRd/yJub90CqoIn2Tz4rRXlSpTFYMKHCifX"
+        crossorigin="anonymous"
+      />
+      <div class="row">
+        <div class="col-sm-12">
+          <h1>Teacher Information</h1>
+          <p>{{ msg }}</p>
+          <button type="button" class="btn btn-info" style="float: left">
+            Add Info
+          </button>
+          <br /><br />
+          <table class="table table-hover">
+            <!--Table Head-->
+            <thead>
+              <tr>
+                <!--Table header cells-->
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Subject</th>
+                <th scope="col">Phone</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>10000</td>
+                <td>Zheng Hao</td>
+                <td>Math</td>
+                <td>
+                  <div class="btn-group" role="group">
+                    <button
+                      type="button"
+                      class="btn btn-success"
+                      style="margin-right: 5px"
+                    >
+                      Update
+                    </button>
+                    <button type="button" class="btn btn-danger">Delete</button>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
