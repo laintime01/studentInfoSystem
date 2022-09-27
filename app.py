3bfa485522009b4de4a81662ef6c486693365e0a
laintime <hzhe111@163.com>
2022-09-27 10:13:19 +0800

    link sqlalchemy to flask

--- app.py
+++ app.py
@@ -3,10 +3,33 @@ from flask_cors import CORS
 import uuid
 # pip install flask-sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
-
+from dataclasses import dataclass
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/sis"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost/sis_db"
+db = SQLAlchemy(app)
+
+
+# create table teacher
+# Python 3.7+ and Flask 1.1+ can use the built-in dataclasses package
+@dataclass
+class Teachers(db.Model):
+    # table name
+    __tablename__ = 'teacher'
+    id: int
+    name: str
+    subject: str
+    phone: str
+
+    # column
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(16), nullable=False)
+    subject = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(20))
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table.columns}
+
 
 app.config.from_object(__name__)
 
@@ -19,31 +42,15 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/test', methods=['GET'])
+def test_func():
+    teachers_info = Teachers.query.all()
+    return jsonify(teachers_info)
+
+
 @app.route('/teacherslogan', methods=['GET'])
 def teacher_slogan():
     return "This is a message for teacher page from flask backend"
-
-
-teachers = [
-    {
-        "id": uuid.uuid4().hex,
-        "name": "Mr Li",
-        "subject": "Math",
-        "phone": "13613239788",
-    },
-    {
-        "id": uuid.uuid4().hex,
-        "name": "Mr Wang",
-        "subject": "EN",
-        "phone": "13613237397",
-    },
-    {
-        "id": uuid.uuid4().hex,
-        "name": "Mr Ho",
-        "subject": "Sports",
-        "phone": "13391379173",
-    },
-]
 
 
 # Add and fetch teacher
@@ -52,15 +59,14 @@ def add_or_fetch_teachers_info():
     response_object = {'status': 'success'}
     if request.method == "POST":
         post_data = request.get_json()
-        teachers.append({
-            "id": uuid.uuid4().hex,
-            "name": post_data.get('name'),
-            "subject": post_data.get('subject'),
-            "phone": post_data.get('phone')}
-        )
+        new_teacher = Teachers(name=post_data.get('name'),
+                               subject=post_data.get('subject'),
+                               phone=post_data.get('phone'))
+        db.session.add(new_teacher)
+        db.session.commit()
         response_object['message'] = post_data.get('name') + ' information added'
     else:
-        response_object['teachers'] = teachers
+        response_object['teachers'] = Teachers.query.all()
     return jsonify(response_object)
 
 
@@ -70,13 +76,12 @@ def del_or_update_teacher(teacher_id):
     response_object = {'status': 'success'}
     if request.method == "PUT":
         post_data = request.get_json()
-        del_teacher(teacher_id)
-        teachers.append({
-            "id": uuid.uuid4().hex,
-            "name": post_data.get('name'),
-            "subject": post_data.get('subject'),
-            "phone": post_data.get('phone')}
-        )
+        Teachers.query.filter(Teachers.id == teacher_id).update({
+            'name': post_data.get('name'),
+            'subject': post_data.get('subject'),
+            'phone': post_data.get('phone')
+        })
+        db.session.commit()
         response_object['message'] = post_data.get('name') + "Teacher Info Updated!"
     if request.method == "DELETE":
         del_teacher(teacher_id)
@@ -86,12 +91,11 @@ def del_or_update_teacher(teacher_id):
 
 def del_teacher(teacher_id):
     print(teacher_id)
-    for t in teachers:
-        if t.get("id") == teacher_id:
-            teachers.remove(t)
-            print(str(t) + "removed")
-            return True
+    d_teacher = Teachers.query.filter(Teachers.id == teacher_id).first()
+    db.session.delete(d_teacher)
+    db.session.commit()
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True, host="localhost", port=8900)
--- create_db.py
+++ create_db.py
@@ -4,5 +4,9 @@ conn = pymysql.connect(host='localhost', user="root", password="")
 cursor = conn.cursor()
 # create database
 cursor.execute('CREATE DATABASE IF NOT EXISTS sis_db DEFAULT CHARSET utf8 COLLATE utf8_general_ci')
-cursor.close()
 print('sis db created')
+cursor.execute('use sis_db')
+cursor.execute('CREATE TABLE IF NOT EXISTS teacher(id int(5) auto_increment primary key, name varchar(255) not null,subject varchar(255) not null,phone varchar(255))')
+print('table teacher created')
+
+
