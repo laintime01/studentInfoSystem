f1e6c3f1f1060904e159474996a51f556b24e320
laintime <hzhe111@163.com>
2022-10-02 17:02:09 +0800

    login vue page done

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,11 +1,33 @@
 <template>
   <b-jumbotron header="Login" lead="welcome to student information system">
     <div class="container">
-      <b-form-group>
-        <b-form-input placeholder="username">Username</b-form-input>
-        <b-form-input placeholder="password">Password</b-form-input>
-        <b-button>Login</b-button>
-      </b-form-group>
+      <b-form @submit="onSubmit">
+        <b-form-group
+          id="username"
+          label-for="input-username"
+          style="margin-top: 30px"
+        >
+          <b-form-input
+            id="input-username"
+            placeholder="Enter username"
+            v-model="loginForm.username"
+            style="width: 60%; margin-top: 30px; display: inline-block"
+            required
+            >Username</b-form-input
+          >
+        </b-form-group>
+        <b-form-group style="margin-top: 20px">
+          <b-form-input
+            id="input-password"
+            placeholder="Enter password"
+            v-model="loginForm.password"
+            required
+            style="width: 60%; margin-top: 20px; display: inline-block"
+            >Password</b-form-input
+          >
+        </b-form-group>
+        <b-button type="submit" style="margin-top: 30px">Login</b-button>
+      </b-form>
     </div>
   </b-jumbotron>
 </template>
@@ -13,6 +35,20 @@
 <script>
 export default {
   name: "LoginView",
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.loginForm));
+    },
+  },
 };
 </script>
 
